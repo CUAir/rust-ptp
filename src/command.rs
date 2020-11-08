5b0299ed59ef0fd02343fc817985cd83028f67b5
Ibiyemi Abiodun <ibiyemi@intulon.com>
2020-11-08 11:47:05 -0500

    Split into modules

---
+++ src/command.rs
@@ -0,0 +1,96 @@
+use std::fmt::{self, LowerHex};
+use num_traits::{FromPrimitive, ToPrimitive};
+
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+pub enum CommandCode {
+    Standard(StandardCommandCode),
+    Other(u16),
+}
+
+impl LowerHex for CommandCode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            CommandCode::Standard(code) => fmt::LowerHex::fmt(code, f),
+            CommandCode::Other(code) => fmt::LowerHex::fmt(code, f),
+        }
+    }
+}
+
+impl FromPrimitive for CommandCode {
+    fn from_i64(n: i64) -> Option<Self> {
+        Some(StandardCommandCode::from_i64(n).map_or_else(
+            || CommandCode::Other(n as u16),
+            |code| CommandCode::Standard(code),
+        ))
+    }
+
+    fn from_u64(n: u64) -> Option<Self> {
+        Some(StandardCommandCode::from_u64(n).map_or_else(
+            || CommandCode::Other(n as u16),
+            |code| CommandCode::Standard(code),
+        ))
+    }
+}
+
+impl ToPrimitive for CommandCode {
+    fn to_i64(&self) -> Option<i64> {
+        match self {
+            CommandCode::Standard(code) => code.to_i64(),
+            CommandCode::Other(code) => Some(*code as i64),
+        }
+    }
+
+    fn to_u64(&self) -> Option<u64> {
+        match self {
+            CommandCode::Standard(code) => code.to_u64(),
+            CommandCode::Other(code) => Some(*code as u64),
+        }
+    }
+}
+
+impl From<StandardCommandCode> for CommandCode {
+    fn from(code: StandardCommandCode) -> Self {
+        CommandCode::Standard(code)
+    }
+}
+
+#[repr(u16)]
+#[derive(FromPrimitive, ToPrimitive, Debug, Copy, Clone, Eq, PartialEq)]
+pub enum StandardCommandCode {
+    Undefined = 0x1000,
+    GetDeviceInfo = 0x1001,
+    OpenSession = 0x1002,
+    CloseSession = 0x1003,
+    GetStorageIDs = 0x1004,
+    GetStorageInfo = 0x1005,
+    GetNumObjects = 0x1006,
+    GetObjectHandles = 0x1007,
+    GetObjectInfo = 0x1008,
+    GetObject = 0x1009,
+    GetThumb = 0x100A,
+    DeleteObject = 0x100B,
+    SendObjectInfo = 0x100C,
+    SendObject = 0x100D,
+    InitiateCapture = 0x100E,
+    FormatStore = 0x100F,
+    ResetDevice = 0x1010,
+    SelfTest = 0x1011,
+    SetObjectProtection = 0x1012,
+    PowerDown = 0x1013,
+    GetDevicePropDesc = 0x1014,
+    GetDevicePropValue = 0x1015,
+    SetDevicePropValue = 0x1016,
+    ResetDevicePropValue = 0x1017,
+    TerminateOpenCapture = 0x1018,
+    MoveObject = 0x1019,
+    CopyObject = 0x101A,
+    GetPartialObject = 0x101B,
+    InitiateOpenCapture = 0x101C,
+}
+
+impl LowerHex for StandardCommandCode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let val = self.to_u16().unwrap();
+        fmt::LowerHex::fmt(&val, f)
+    }
+}
---
+++ src/response.rs
@@ -0,0 +1,84 @@
+use std::fmt::{self, LowerHex};
+use num_traits::{FromPrimitive, ToPrimitive};
+
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+pub enum ResponseCode {
+    Standard(StandardResponseCode),
+    Other(u16),
+}
+
+impl FromPrimitive for ResponseCode {
+    fn from_i64(n: i64) -> Option<Self> {
+        Some(StandardResponseCode::from_i64(n).map_or_else(
+            || ResponseCode::Other(n as u16),
+            |code| ResponseCode::Standard(code),
+        ))
+    }
+
+    fn from_u64(n: u64) -> Option<Self> {
+        Some(StandardResponseCode::from_u64(n).map_or_else(
+            || ResponseCode::Other(n as u16),
+            |code| ResponseCode::Standard(code),
+        ))
+    }
+}
+
+impl LowerHex for ResponseCode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ResponseCode::Standard(code) => fmt::LowerHex::fmt(code, f),
+            ResponseCode::Other(code) => fmt::LowerHex::fmt(code, f),
+        }
+    }
+}
+
+impl From<StandardResponseCode> for ResponseCode {
+    fn from(code: StandardResponseCode) -> Self {
+        ResponseCode::Standard(code)
+    }
+}
+
+#[repr(u16)]
+#[derive(FromPrimitive, ToPrimitive, Debug, Copy, Clone, Eq, PartialEq)]
+pub enum StandardResponseCode {
+    Undefined = 0x2000,
+    Ok = 0x2001,
+    GeneralError = 0x2002,
+    SessionNotOpen = 0x2003,
+    InvalidTransactionId = 0x2004,
+    OperationNotSupported = 0x2005,
+    ParameterNotSupported = 0x2006,
+    IncompleteTransfer = 0x2007,
+    InvalidStorageId = 0x2008,
+    InvalidObjectHandle = 0x2009,
+    DevicePropNotSupported = 0x200A,
+    InvalidObjectFormatCode = 0x200B,
+    StoreFull = 0x200C,
+    ObjectWriteProtected = 0x200D,
+    StoreReadOnly = 0x200E,
+    AccessDenied = 0x200F,
+    NoThumbnailPresent = 0x2010,
+    SelfTestFailed = 0x2011,
+    PartialDeletion = 0x2012,
+    StoreNotAvailable = 0x2013,
+    SpecificationByFormatUnsupported = 0x2014,
+    NoValidObjectInfo = 0x2015,
+    InvalidCodeFormat = 0x2016,
+    UnknownVendorCode = 0x2017,
+    CaptureAlreadyTerminated = 0x2018,
+    DeviceBusy = 0x2019,
+    InvalidParentObject = 0x201A,
+    InvalidDevicePropFormat = 0x201B,
+    InvalidDevicePropValue = 0x201C,
+    InvalidParameter = 0x201D,
+    SessionAlreadyOpen = 0x201E,
+    TransactionCancelled = 0x201F,
+    SpecificationOfDestinationUnsupported = 0x2020,
+}
+
+impl LowerHex for StandardResponseCode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let val = self.to_u16().unwrap();
+        fmt::LowerHex::fmt(&val, f)
+    }
+}
